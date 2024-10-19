2276fe9a285a0e0f80a580917df5671eb19f695b
opo <opo@local>
2024-10-19 21:39:47 +0300

    -- wip

--- src/client/components/content/MediaList.tsx
+++ src/client/components/content/MediaList.tsx
@@ -4,14 +4,15 @@ import MediaPlayer, { Direction } from "./player/Player";
 import { EntryData, useMp3Context } from "../../context/appContext";
 import { createIntersectionObserver } from "@solid-primitives/intersection-observer";
 
-import './media_list.scss';
+import "./media_list.scss";
 
 export default function MediaList() {
-    const [pick, setNewEntry] = createSignal<EntryData|undefined>();
+    const [pick, setNewEntry] = createSignal<EntryData | undefined>();
     const [visible, setVisible] = createSignal<string[]>([]);
-    const [activeDetail, setActiveDetail] = createSignal<EntryData|undefined>();
-    const {content, serverMessage, refetchContent, cleanup} = useMp3Context();
-    
+    const [activeDetail, setActiveDetail] = createSignal<
+        EntryData | undefined
+    >();
+    const { content, serverMessage, refetchContent, cleanup } = useMp3Context();
 
     const [targets, setTargets] = createSignal<Element[]>([]);
 
@@ -21,63 +22,70 @@ export default function MediaList() {
         } else {
             setActiveDetail(entry);
         }
-    }
+    };
 
     onMount(() => {
-        const {innerWidth, innerHeight} = window;
-        console.log('--here ', innerHeight, innerWidth)
+        const { innerWidth, innerHeight } = window;
+        console.log("--here ", innerHeight, innerWidth);
     });
 
     createIntersectionObserver(targets, (entries, observer) => {
-        entries.forEach(e => {
+        entries.forEach((e) => {
             if (e.isIntersecting) {
-                let pic = e.target.getElementsByTagName('picture')[0];
-                pic.getElementsByTagName('source')[0].srcset = pic.dataset.url!;
-                pic.getElementsByTagName('img')[0].src = pic.dataset.url!;
+                let pic = e.target.getElementsByTagName("picture")[0];
+                pic.getElementsByTagName("source")[0].srcset = pic.dataset.url!;
+                pic.getElementsByTagName("img")[0].src = pic.dataset.url!;
                 observer.unobserve(e.target);
             }
-        })
-      });
+        });
+    });
 
     function changeDirection(flow: Direction) {
-        if (flow === 'stop') {
+        if (flow === "stop") {
             setNewEntry(undefined);
             return;
         }
-        const playIndex = content()!.findIndex(entry => entry.media_id === pick()?.media_id)
+        const playIndex = content()!.findIndex(
+            (entry) => entry.media_id === pick()?.media_id,
+        );
         if (playIndex === -1) {
             return;
         }
 
-        const direction = flow === 'next' ? 1 : -1;
-        let newIndex =playIndex + direction;
+        const direction = flow === "next" ? 1 : -1;
+        let newIndex = playIndex + direction;
         if (newIndex <= 0) {
             newIndex = content()!.length - 1;
-        } else  if (newIndex>= content()!.length) {
+        } else if (newIndex >= content()!.length) {
             newIndex = 0;
         }
         setNewEntry(content()![newIndex]);
     }
 
     createEffect(() => {
-        if (serverMessage()?.includes('refresh content')) {
-            console.log(serverMessage(), 'refreshing content')
+        if (serverMessage()?.includes("refresh content")) {
+            console.log(serverMessage(), "refreshing content");
             refetchContent();
         }
-    })
+    });
 
     onCleanup(cleanup);
 
     createEffect(() => {
-        console.log('visible targets', visible())
-    })
+        console.log("visible targets", visible());
+    });
 
-    return(
+    const fewMedia = () => {
+        if (!content()) return true;
+        return content()!.length < 3;
+    };
+
+    return (
         <>
-            <MediaPlayer pick={pick} action={changeDirection}/>
-            <ul class='media-list' >
+            <MediaPlayer pick={pick} action={changeDirection} />
+            <ul class="media-list" classList={{ small: fewMedia() }}>
                 <For each={content()}>
-                    {entry => (
+                    {(entry) => (
                         <MediaEntry
                             changeMedia={setNewEntry}
                             entry={entry}
